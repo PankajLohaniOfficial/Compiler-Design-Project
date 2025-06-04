137593a352d3e54cad4f674e89f4d5680ed23098
PL <109916837+PankajLohaniOfficial@users.noreply.github.com>
2025-06-04 07:45:48 +0530

    pyqt.sh

--- astviewer/tools/pyqt.sh
+++ astviewer/tools/pyqt.sh
@@ -1,8 +1,6 @@
-#! /usr/bin/env bash                     
-#tells os to run script using bash shell
+#! /usr/bin/env bash
 
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+project_dir="$script_dir/.."
 
 pyuic4 "$project_dir/ui/mainwindow.ui" -o "$project_dir/ui/mainwindow.py"
-
-# After running this script, you’ll get a mainwindow.py file that contains Python classes corresponding to the GUI you designed in Qt Designer.
